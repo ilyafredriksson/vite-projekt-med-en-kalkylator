a010c1ae3c95470bd078619298eeb25c43586e7a
ilyafredriksson <fredrikssonilya@gmail.com>
2025-04-30 13:04:42 +0200

    första commit -kalkyljator

---
+++ src/calculator.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+    return a + b;
+  }
+  
+  export function subtract(a, b) {
+    return a - b;
+  }
+  
+  export function multiply(a, b) {
+    return a * b;
+  }
+  
+  export function divide(a, b) {
+    if (b === 0) throw new Error("Kan inte dividera med 0");
+    return a / b;
+  }
+  
